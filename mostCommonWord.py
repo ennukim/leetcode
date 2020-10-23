57c071e5d5ba397dcbc948b3fd3cb4846ce90b76
sally kim <sallykim@sallys-MBP-1146.fios-router.home>
2020-10-23 02:16:02 -0400

    Oct 23, 2020

---
+++ mostCommonWord.py
@@ -0,0 +1,24 @@
+def mostCommonWord(paragraph, banned):
+    normalized_str = ''.join([c.lower() if c.isalnum() else ' ' for c in paragraph])
+    words = normalized_str.split()
+    hash_banned = set(banned)
+    hash_para = dict()
+
+    for i in range(len(words)):
+        if words[i] not in hash_para and words[i] not in hash_banned:
+            hash_para[words[i]] = 1
+        if words[i] in hash_para:
+            hash_para[words[i]] += 1
+        else:
+            pass
+
+    key_list = list(hash_para.keys()) 
+    val_list = list(hash_para.values()) 
+
+    return key_list[val_list.index(max(val_list))]
+
+# paragraph = "a, a, a, a, b,b,b,c, c"
+# banned = ["a"]
+paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+banned = ["hit"]
+print(mostCommonWord(paragraph, banned))
