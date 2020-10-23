873ae14e13080d139b097f276f3ca68065161712
sally kim <sallykim@sallys-MBP-1146.fios-router.home>
2020-10-22 23:34:07 -0400

    commit!

--- 3sum.py
+++ 3sum.py
@@ -4,31 +4,33 @@ Tried again on Tue Oct 20, 2020
 @author: sallykim
 """
 
-#initial try
-#pointer function - O(n)
-#for loop - n times
-#sort() - O(nlogn)
-#Total - O(nlogn + n^2)
+#   My initial try
+#   Time complexity - O(nlogn + n^2)
+#       sort() - O(nlogn)
+#       pointer function - O(n)
+#       for loop - n times 
+
 def threeSum(nums):
     res = []
     nums.sort()
     length = len(nums)
     for i in range(length-2):
         if nums[i] > 0: break
+        #   the second comparison condition avoids 
+        #   an unnecessary run that has been done previously
         if i > 0 and nums[i]==nums[i-1]: continue
     
         left, right = i+1, length-1
         while left < right:
             total = nums[i]+nums[left]+nums[right]
-            
             if total < 0:
-                left += 1
-                
+                left += 1 
             elif total > 0:
                 right -= 1
-            
             else:
                 res.append([nums[i],nums[left],nums[right]])
+                #   the second comparison condition avoids 
+                #   an unnecessary run that has been done previously
                 while left < right and nums[left]==nums[left+1]:
                     left+=1
                 while left < right and nums[right]==nums[right-1]:
@@ -41,7 +43,6 @@ def threeSum(nums):
 #no sorting version
 def threeSum2(nums):
     
-
+#nums = [-2, 0, 0, 2, 2]
 nums = [-1, 0, 1, 2, -1, -4]
 print(threeSum2(nums))
-
