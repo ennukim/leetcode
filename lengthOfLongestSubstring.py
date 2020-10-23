96f54e5fbdd1eaa0ee82bd3f21e4f6f29d99879c
sally kim <sallykim@sallys-MBP-1146.fios-router.home>
2020-10-22 22:47:31 -0400

    Oct 22, 2020

---
+++ lengthOfLongestSubstring.py
@@ -0,0 +1,26 @@
+#   Longest Substring Without Repeating Characters
+#   Amazon Interview Question
+
+#   ******Sliding Window Apprach******
+#   Time complexity : O(2n) = O(n). 
+#   In the worst case each character will be visited twice by i and j.
+def lengthOfLongestSubstring(s):
+    char_set = set()
+    n = len(s)
+    ans = 0
+    i = 0
+    j = 0
+    while i < n and j < n:
+        if(s[j] not in char_set):
+            char_set.add(s[j])
+            j += 1
+            ans = max(ans, j - i)
+        else:
+            i += 1
+            char_set.remove(s[i])
+    return ans
+
+s = "abcabcbb"
+#s = "bbbbb"
+#s = "pwwkew"
+print(lengthOfLongestSubstring(s))
