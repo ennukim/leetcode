9332a06eff3653df48d80bbb3f0ba8ff9d44fa53
sally kim <sallykim@sallys-MBP-1146.fios-router.home>
2020-10-28 17:31:13 -0400

    Oct 28, 2020

---
+++ myAtoi.py
@@ -0,0 +1,17 @@
+#String to Integer (atoi)
+#Amazon Interview Question
+
+def myAtoi(s):
+    sp = s.split(" ")
+
+
+    print(sp)
+    print(new_str)
+
+
+
+
+str = "   -42"
+#str = "4193 with words"
+#str = "words and 987"
+print(myAtoi(str))
