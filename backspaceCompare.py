bfe0df9e786274d8051a09d96642997197f85c3d
sally kim <sallykim@sallys-MBP-1146.fios-router.home>
2020-11-11 01:20:08 -0500

    Nov 10, 2020

---
+++ backspaceCompare.py
@@ -0,0 +1,36 @@
+# Backspace String Compare
+# Google interview - Arrays & Strings
+
+def backspaceCompare(S, T):
+    count = 0
+    ns = ""
+    nt = ""
+    for i in range(len(S)-1,-1,-1):
+        if S[i] == "#":
+            count += 1
+        elif S[i].isalpha() and not count:
+            ns += S[i]
+        elif S[i].isalpha():
+            count -= 1
+    count = 0
+    for i in range(len(T)-1,-1,-1):
+        if T[i] == "#":
+            count += 1
+        elif T[i].isalpha() and not count:
+                nt += T[i]
+        elif T[i].isalpha():
+            count -= 1
+        
+    # print(ns, nt)
+    return ns == nt
+
+# S = "ab#c"
+# T = "ad#c"
+S = "a#c"
+T = "b"
+
+# S = "a##c"
+# T = "#a#c"
+# S = "ab##"
+# T = "c#d#"
+print(backspaceCompare(S, T))
