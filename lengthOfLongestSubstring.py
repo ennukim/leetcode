03cccdc84baff73d681ae3fa2d4d714225957f21
ennukim <ek2777@nyu.edu>
2020-11-13 20:20:20 -0500

    Update lengthOfLongestSubstring.py

--- lengthOfLongestSubstring.py
+++ lengthOfLongestSubstring.py
@@ -21,6 +21,20 @@ def lengthOfLongestSubstring(s):
             i += 1
     return ans
 
+
+#   Time complexity : O(n)
+def lengthOfLongestSubstring2(s):
+    used = {}
+    max_length = start = 0
+    for i, c in enumerate(s):
+        if c in used and start <= used[c]:
+            start = used[c] + 1
+        else:
+            max_length = max(max_length, i - start + 1)
+        used[c] = i
+    return max_length
+
+
 s = "abcabcbb"
 #s = "bbbbb"
 #s = "pwwkew"
